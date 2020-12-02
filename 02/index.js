585c1beda857539f3598a8d90cde94fe4c696ce8
Stanley Tiu <stanleytiu7@gmail.com>
2020-12-02 00:51:24 -0500

    fix the code

--- 02/index.js
+++ 02/index.js
@@ -1,7 +1,7 @@
 const { getParsedData } = require('../utils');
 const data = getParsedData('./data')
 
-function parseEntry(entry) {
+function parseEntry(entry, fn) {
     if (!entry) return
     const split = entry.split(':');
     const rules = split[0];
@@ -9,7 +9,7 @@ function parseEntry(entry) {
     if (!password) throw new Error(password);
 
     const [min, max, rule] = parseRules(rules)
-    return validateTwo(min, max, rule, password)
+    return fn(min, max, rule, password)
 }
 
 function parseRules(rules) {
@@ -30,17 +30,20 @@ function validateOne(min, max, rule, password) {
 }
 
 
+// exactly 1 occurence of the letter between the two posistions
 // 1-3 a: abcde is valid: position 1 contains a and position 3 does not.
 // 1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
 // 2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
 // index by 1 not 0
 function validateTwo(min, max, rule, password) {
-    return password[min] === rule && password[max] !== rule 
-    ||password[min] !== rule && password[max] === rule
+    const isFirst = password[min] === rule && password[max] !== rule 
+    const isSecond = password[min] !== rule && password[max] === rule
+
+    return isFirst || isSecond
 }
 
 const validNumber = data.reduce((sum, entry) => {
-    if (parseEntry(entry)) return sum + 1
+    if (parseEntry(entry, validateTwo)) return sum + 1
     else return sum
 }, 0);
 
