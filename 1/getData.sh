1c11494422bb8d07dcee4254db3d88c184524792
Stanley Tiu <stanleytiu7@gmail.com>
2020-12-01 23:41:41 -0500

    remove secrets

--- 1/getData.sh
+++ 1/getData.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-URL=https://adventofcode.com/2020/day/1/input
-session=53616c7465645f5fb1873c30a1e05b57e956fd7e515b48d08103ace1ab6b7cac60a14e92dbb599d195c1ff8c9c80716f
+URL=$1
+session=$2
 
 curl -o data --cookie "session=$session" $URL
