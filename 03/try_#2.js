b5c9e78f39ebe93669d32317d4bec3d4b9364504
Stanley Tiu <stanleytiu7@gmail.com>
2020-12-03 01:40:10 -0500

    Update try_#2.js

--- 03/try_#2.js
+++ 03/try_#2.js
@@ -8,13 +8,16 @@ function treeSum(data, x_slope, y_slope) {
     let sum = 0;
     let x = 0;
     for (let y = 0; y < data.length; y-=y_slope) {
-        sum += data[y][x % data[y].length] === '#'
-        x+=x_slope
+        const row = data[y];
+        sum += row[x % row.length] === '#';
+        x += x_slope;
     }
-    return sum
+    return sum;
 }
  
 for (const pair of [[1,-1], [3,-1], [5,-1],[7,-1],[1,-2]]) {
     let product = 1;
-    product *= treeSum(data, pair[0], pair[1])
+    product *= treeSum(data, pair[0], pair[1]);
 }
+
+console.log(product)
