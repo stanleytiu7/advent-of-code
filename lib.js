51dc178d47e35a54cff3830cd5f6e07a406a8f18
Stanley Tiu <stanleytiu7@gmail.com>
2020-12-04 02:38:54 -0500

    change default to be run in directory instead of anywhere

--- lib.js
+++ lib.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs');
 
 function getRawInput() {
-        const inputFilename = path.resolve(process.cwd(), process.argv[1],
+        const inputFilename = path.resolve(process.cwd(),
             path.basename('data.txt'))
                     // path.basename(process.argv[1], '.js').replace(/\D+$/, '') + '.txt');
         const input = fs.readFileSync(inputFilename, 'utf-8').trim();
