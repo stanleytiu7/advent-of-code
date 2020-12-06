7dc13002e43929973aafb0091ddc047568a795a0
Stanley Tiu <stanleytiu7@gmail.com>
2020-12-06 01:25:00 -0500

    day 6

---
+++ 2020/06.js
@@ -0,0 +1,33 @@
+const { getRawInput } = require('../lib');
+
+const unparsed = getRawInput();
+const data = unparsed.split('\n\n');
+
+const test = 'abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb'.split('\n\n')
+
+run(data);
+
+function run(data) {
+    let sum = 0;
+    for (const group of data) {
+        sum += parseGroup(group);
+    }
+    console.log(sum)
+}
+
+function parseGroup(group) {
+    const cache = {}
+    group = group.split('\n')
+    for (const person of group) {
+        for (const question of person) {
+            if (!cache[question]) cache[question] = 1
+            else cache[question]++
+        }
+    }
+    
+    let returned = 0;
+    for (const key of Object.keys(cache)) {
+        if (cache[key] === group.length) returned++
+    }
+    return returned
+}
